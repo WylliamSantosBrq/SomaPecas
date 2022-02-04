a60e3b02be34ffb600814a8f310415ae977896e2
Josue Silva de Lima <josuelima@brq.com>
2022-02-04 10:45:33 -0300

    aba Aviso de Sinistro

--- src/test/java/com/brq/elementos/abas/AbasMenuElementos.java
+++ src/test/java/com/brq/elementos/abas/AbasMenuElementos.java
@@ -9,7 +9,9 @@ public class AbasMenuElementos {
 	private AbasMenuElementos() {
 	}
 	
-	public static final Botao BTN_AVISO_DE_SINISTRO = new Botao(By.xpath("//a[contains(@id, 'Menu') and text()='Aviso de Sinistro']"));
+	//public static final Botao BTN_AVISO_DE_SINISTRO = new Botao(By.xpath("//a[contains(@id, 'Menu') and text()='Aviso de Sinistro']"));
+	public static final Botao BTN_AVISO_DE_SINISTRO = new Botao(By.id("identifierAMenu2"));
+
 	public static final Botao BTN_AS_CONSULTAS = new Botao(By.xpath("//div[contains(@onmouseout, 'Menu2')]//a[text()='Consultas']"));
 	public static final Botao BTN_AS_CONS_NUMERO_DO_SINISTRO = new Botao(By.xpath("//div[contains(@onmouseout, 'Menu2')]//tr[.//a[text()='Consultas']][1]/following::tr//a[text()='Número do Sinistro']"));
 
