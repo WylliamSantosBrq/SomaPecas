14e3d421ec47005fdf059efc7af9f3299ac02411
Josue Silva de Lima <josuelima@brq.com>
2022-02-11 15:48:51 -0300

    alteração DriverChome

--- src/main/java/com/brq/drivers/web/DriverChrome.java
+++ src/main/java/com/brq/drivers/web/DriverChrome.java
@@ -2,6 +2,7 @@ package com.brq.drivers.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
