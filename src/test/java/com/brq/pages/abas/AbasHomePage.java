931a9a28252beff021da03517c32d620b84f65d5
Josue Silva de Lima <josuelima@brq.com>
2022-02-11 17:04:22 -0300

    test URL

--- src/test/java/com/brq/pages/abas/AbasHomePage.java
+++ src/test/java/com/brq/pages/abas/AbasHomePage.java
@@ -1,17 +1,38 @@
 package com.brq.pages.abas;
 
+import org.openqa.selenium.WebDriver;
+
+import com.brq.drivers.web.DriverWeb;
 import com.brq.interfaces.abas.AbasMenuSuperior;
 import com.brq.interfaces.log.BRQLogger;
 import com.brq.interfaces.web.IEsperaWeb;
+import com.brq.interfaces.web.IValidacoesBrowser;
 import com.brq.pages.Pagina;
 
 public class AbasHomePage extends Pagina implements AbasMenuSuperior {
 
 	public AbasHomePage() {
 		BRQLogger.logMethod();
-		//trocarJanelaPorIndice(0);
-		IEsperaWeb.esperarJanelaComTitulo("Aviso de Sinistro", 30);
+		trocarJanelaPorIndice(0);
+		
+		BRQLogger.logMethod();
+		trocarJanelaPorIndice(0);
+		WebDriver driver = DriverWeb.getDriver();
+		String urlOriginal = driver.getCurrentUrl();
+
+		String novaURL = null;
+		if ( urlOriginal.contains("apphubhml")) {
+
+		novaURL = urlOriginal.replace("apphubhml", "apphubhml.portoseguro.brasil");
+		}
+
+		abrirUrl(novaURL);
+
 		esperarPaginaAbrirContains("Aviso de Sinistro", 30);
+		}
+		
+//		IEsperaWeb.esperarJanelaComTitulo("Aviso de Sinistro", 30);
+//		esperarPaginaAbrirContains("Aviso de Sinistro", 30);
 	}
 
-}
+
