32ed0862ae403eaf38a27c55d779af53b3c313ef
Josue Silva de Lima <josuelima@brq.com>
2022-02-22 10:45:41 -0300

    atualização CT005

--- src/test/java/com/brq/pages/abas/AbasHomePage.java
+++ src/test/java/com/brq/pages/abas/AbasHomePage.java
@@ -1,56 +1,17 @@
 package com.brq.pages.abas;
 
-import java.text.MessageFormat;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.openqa.selenium.WebDriver;
-
-import com.brq.drivers.web.DriverWeb;
 import com.brq.interfaces.abas.AbasMenuSuperior;
 import com.brq.interfaces.log.BRQLogger;
+import com.brq.interfaces.web.IEsperaWeb;
 import com.brq.pages.Pagina;
 
 public class AbasHomePage extends Pagina implements AbasMenuSuperior {
 
 	public AbasHomePage() {
 		BRQLogger.logMethod();
-		trocarJanelaPorIndice(0);
-		WebDriver driver = DriverWeb.getDriver();
-		
-		String urlOriginal;
-		do{
-		urlOriginal = driver.getCurrentUrl();
-		}
-		while(urlOriginal==null);
-		
-		
-        System.out.println(MessageFormat.format("URL:{0}",urlOriginal));
-        
-        Set<String> janelas = DriverWeb.getDriver().getWindowHandles(),urls = new HashSet<>();
-        for(String janela:janelas)       {
-                driver.switchTo().window(janela);
-                System.out.println(MessageFormat.format("Janela encontrada:{0}\nTítulo:{1}\nURL:{2}",//
-                janela,driver.getTitle(),driver.getCurrentUrl()));
-                  urls.add(driver.getCurrentUrl());
-            }
-       urlOriginal = urls.stream()//
-       .filter(x -> x.contains("apphub"))//
-       .findFirst()//
-       .get();
-        
-        System.out.println(MessageFormat.format("URL:{0}",urlOriginal));
-        String novaURL = null;
-		if ( urlOriginal.contains("apphubhml")) {
-		novaURL = urlOriginal.replace("apphubhml", "apphubhml.portoseguro.brasil");
-		}
-       System.out.println(MessageFormat.format("URL:{0}",novaURL));
-		abrirUrl(novaURL);
-		esperarPaginaAbrirContains("Aviso de Sinistro", 60);
-		}
-		
-//		IEsperaWeb.esperarJanelaComTitulo("Aviso de Sinistro", 30);
-//		esperarPaginaAbrirContains("Aviso de Sinistro", 30);
+		//trocarJanelaPorIndice(0);
+		IEsperaWeb.esperarJanelaComTitulo("Aviso de Sinistro", 30);
+		esperarPaginaAbrirContains("Aviso de Sinistro", 30);
 	}
 
-
+}
