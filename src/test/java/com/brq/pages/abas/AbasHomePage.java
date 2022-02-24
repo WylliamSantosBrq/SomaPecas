74669e29c0ef1b47f2639a432c103c55a955ca9f
LUANA APARECIDA SILVA <luana.aparecida@portoseguro.com.br>
2022-02-24 15:02:23 -0300

    Ajustando para ver o caso de url na troca de página

--- src/test/java/com/brq/pages/abas/AbasHomePage.java
+++ src/test/java/com/brq/pages/abas/AbasHomePage.java
@@ -5,11 +5,27 @@ import com.brq.interfaces.log.BRQLogger;
 import com.brq.pages.Pagina;
 
 public class AbasHomePage extends Pagina implements AbasMenuSuperior {
+    Logger logger = Logger.getLogger("AbasHomePage");
 
 	public AbasHomePage() {
 		BRQLogger.logMethod();
+		try{
 		trocarJanelaPorIndice(0);
 		esperarPaginaAbrirContains("Aviso de Sinistro", 30);
+		}
+		catch(Throwable e)
+		{
+		    logger.error(e.getMessage());
+		    String url = "";
+		    for(String hand:getHandles())
+		    {
+		        trocarJanela(hand);
+		        url = getUrl();
+		        logger.debug(url);
+		        logger.debug(getTitle());
+		    }
+		    esperarPadrao(999);
+		}
 	}
 
 }
